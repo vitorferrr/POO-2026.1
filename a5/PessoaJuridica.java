bf767446248d211101353f3a44195584b6dc4305
Vitor Ferreira <vitorferr1412@gmail.com>
2026-03-13 18:48:34 -0300

    aula 5 - heranças

---
+++ a5/PessoaJuridica.java
@@ -0,0 +1,38 @@
+package a5;
+
+public class PessoaJuridica extends Pessoa{
+    private String cnpj;
+
+    public PessoaJuridica(String cnpj, String nome, int idade){
+        super(nome, idade);
+        this.cnpj = cnpj;
+    }
+
+    @Override
+    public int getIdade() {
+        return super.getIdade();
+    }
+
+    @Override
+    public String getNome() {
+        return super.getNome();
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    @Override
+    public void setIdade(int idade) {
+        super.setIdade(idade);
+    }
+
+    @Override
+    public void setNome(String nome) {
+        super.setNome(nome);
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
